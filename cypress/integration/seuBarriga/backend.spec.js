8528d893c034866e81b13a3e87786058e1a15d6a
Siqueira Neto <siqueiranetoufc@gmail.com>
2024-01-02 11:34:39 -0300

    criacao teste api adicionando conta

--- cypress/integration/seuBarriga/backend.spec.js
+++ cypress/integration/seuBarriga/backend.spec.js
@@ -26,7 +26,26 @@ describe('Should test at a funcional level', () =>{
             senha: "1234"
         }
       }).its('body.token').should('not.be.empty')
-    })
+      .then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: "conta VIA REST"
+                    }
+            }).as('response')
+        })
+
+        cy.get('@response').then(res =>{
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'conta VIA REST')
+        })
+
+      })
+
+      
 
     //it('Should update an account', () => {
       
